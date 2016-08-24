c267992db1ce498f71421b95e2879aca5cb56fc1
songjinzhong <648660165@qq.com>
2016-08-24 21:20:31 +0800

    fix gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,10 @@
 var gulp = require('gulp');
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
 
-
-gulp.task('default', function () {
-    // place code for your default task here
-
-});
+gulp.task('default', ['song_scss','song_watch']);
 
 gulp.task('bao-1-8', function () {
     return gulp.src('./stage1/task8/bao/sass/**/*.sass')
@@ -35,3 +33,22 @@ gulp.task('bao-1-10', function () {
 gulp.task('bao-1-10:watch', function () {
     gulp.watch('./stage1/task10/bao/sass/**/*.sass', ['bao-1-10']);
 });
+
+/**
+ * song contron here
+ */
+ var song_paths={
+    task8_scss:"stage1/task8/song/scss/*.scss",
+    task8_scss_dest:"stage1/task8/song/css"
+ }
+ gulp.task("song_scss",function(){
+    gulp.src(song_paths.task8_scss)
+        .pipe(sass())
+        .pipe(gulp.dest(song_paths.task8_scss_dest))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest(song_paths.task8_scss_dest));
+ });
+ gulp.task("song_watch",function(){
+    gulp.watch(song_paths.task8_scss, ['song_scss']);
+ });
