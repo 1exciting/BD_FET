a1b849172230449cb1afa61a9a145b56d14a2f0e
haven.bao <baoyx007@gmail.com>
2016-08-24 10:54:01 +0800

    add gulp support

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
     + [x] task3
     + [x] task4
     + [x] task6
-    + [ ] [task7](http://ife.baidu.com/task/detail?taskId=7)
+    + [x] [task7](http://ife.baidu.com/task/detail?taskId=7)
         * [demo](http://htmlpreview.github.io/?https://github.com/1exciting/BD_FET/blob/master/stage1/task7/index.html)
-    + [ ] task8
+    + [ ] [task8](http://ife.baidu.com/task/detail?taskId=8)
     + [ ] task9
     
     
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by haven on 16/8/24.
+ */
+var gulp = require('gulp');
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('default', function () {
+    // place code for your default task here
+
+});
+
+gulp.task('bao-1-8', function () {
+    return gulp.src('./stage1/task8/bao/sass/**/*.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./stage1/task8/bao/css/'));
+});
+
+gulp.task('bao-1-8:watch', function () {
+    gulp.watch('./stage1/task8/sass/**/*.scss', ['bao-1-8']);
+});
